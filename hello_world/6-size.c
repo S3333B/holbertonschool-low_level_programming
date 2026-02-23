c7c4f9890a748d333c9d17edbc86ea08f1f01f90
S3333B <seb.lamblin59@gmail.com>
2026-02-23 15:17:14 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,5 +11,10 @@ int main(void)
 	printf("The size of a long int: %zu\n", sizeof(long int));
 	printf("The size of a long long int: %zu\n", sizeof(long long int));
 	printf("The size of a float: %zu\n", sizeof(float));
+	printf("The size of a char: %zu\n", sizeof(char));
+	printf("The size of a int: %zu\n", sizeof(int));
+	printf("The size of a long int: %zu\n", sizeof(long int));
+	printf("The size of a long long int: %zu\n", sizeof(long long int));
+	printf("The size of a float: %zu\n", sizeof(float));
 	return (0);
 }
