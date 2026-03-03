8a02632fe27c7c0465b9b55680f1cd5299efb3a3
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-03-03 12:44:53 +0100

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,8 @@
+#include "main.h"
+
 /**
  * reset_to_98 - updates the value pointed to by n to 98
- * @n: pointer to the integer to update
+ * @n: pointer to an integer
  */
 void reset_to_98(int *n)
 {
