dfcc98675d281359cda1b12eb393d5fe9003a241
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-03-06 08:08:49 +0100

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
+char *cap_string(char *);
 void reverse_array(int *a, int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
