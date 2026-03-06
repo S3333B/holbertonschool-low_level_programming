0977d3c9b2ee13756f62da4f6b677cfc87de0b76
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-03-06 07:57:49 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,15 +5,18 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: negative, zero, or positive difference like strcmp
+ * Return: negative if s1 < s2, positive if s1 > s2, 0 if equal
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
+	}
 
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (s1[i] - s2[i]);
 }
