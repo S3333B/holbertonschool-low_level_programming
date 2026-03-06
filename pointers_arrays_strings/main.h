232ee0e7731b468b0a0a20b8180155f35ad8840a
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-03-06 09:09:03 +0100

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
